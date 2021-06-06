74ebf13b4ee55fd64bf08a28886cbe915af78eb5
Adil Hossain <38645490+Adilhossain227@users.noreply.github.com>
2021-06-06 13:11:31 +0600

    main python file v2

--- main.py
+++ main.py
@@ -4,16 +4,31 @@ import time
 ser = serial.Serial('COM8', 9600, timeout=0, parity=serial.PARITY_NONE,
 
                     stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
-Y='LOADING'
+var1=''
+var2=''
+var3=''
+var4=''
+var5=''
 while True:
+    count =0
     nbytes = ser.inWaiting()
     x = str(ser.readline(nbytes))
     x = x.lstrip("b")
     x = x.strip("'")
     x = x.rstrip("\\r\\n")
-    #print(x)        #to print what python has received
-    y=x.split(',') # to split the received string into , delimeter separated array
-    print(y[0]) # print the first char in array as value
+    #print(x)
+    y=x.split(',')
+    for a in y[0:1]:
+        var1=a
+    for a in y[1:2]:
+        var2=a
+    for a in y[2:3]:
+        var3=a
+    for a in y[3:4]:
+        var4=a
+    for a in y[4:5]:
+        var5=a
+    print(var1,var2,var3,var4,var5)
     ser.flushInput()
-    time.sleep(0.6)
+    time.sleep(0.5)
 ser.close()
